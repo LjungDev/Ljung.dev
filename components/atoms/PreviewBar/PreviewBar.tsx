a72f35eb0278fbb0f4446c16fd894d53d0d814a7
Ljung <me@ljung.dev>
2022-08-25 23:40:07 +0200

    ✨ Feat(preview): add disable button to PreviewBar

--- components/atoms/PreviewBar/PreviewBar.tsx
+++ components/atoms/PreviewBar/PreviewBar.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function PreviewBar(): JSX.Element {
   return (
-    <div className="w-full bg-aurora-0 text-snow-storm-2 text-center text-sm p-1">
+    <div className="w-full bg-aurora-0 text-snow-storm-2 text-center text-sm p-1 relative">
       <span>Preview Mode</span>
+
+      <div className="absolute right-0 top-0 h-full w-5 flex justify-center items-center">
+        <Link href="/api/preview?clear=true">
+          <a className="text-frost-3 dark:text-frost-1 hover:opacity-50 active:opacity-75">
+            X
+          </a>
+        </Link>
+      </div>
     </div>
   );
 }
